42544d00e3eb6d1e6272076eec4ed1272d7186ea
dom000 <godknowsegiboy00@gmail.com>
2022-07-16 11:11:51 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold text-blue-700 underline">
+      Hello world!
+    </h1>
+  )
+}
