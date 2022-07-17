9d4763ed09f457ef8bca618a1b670d3d9dcc47e1
dom000 <godknowsegiboy00@gmail.com>
2022-07-17 22:38:07 +0100

    jjji

---
+++ pages/resume.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function resume() {
+  return (
+    <div className='flex justify-center items-center p-10'><img src='/Godknows@Cv.png' className='object-contain' alt='resume'/></div>
+  )
+}
+
+export default resume
